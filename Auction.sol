0ba93735179af57d27607dae2726ddbd4d122c91
hritika-rai <hritikarathi08@gmail.com>
2023-12-16 12:02:43 +0500

    Update Auction.sol

--- Auction.sol
+++ Auction.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity ^0.8.0;
 
 contract DecentralizedAuction {
@@ -7,20 +6,23 @@ contract DecentralizedAuction {
     uint256 public startTime;
     uint256 public endTime;
     uint256 public highestBindingBid;
-    address public highestBidder;
-
+    uint256 public numHighestBidders;
+    mapping(uint256 => address) public highestBidders;
     mapping(address => uint256) public bids;
-
     address[] public bidders;
+    address winner;
+
 
     constructor(uint256 _durationMinutes) {
         owner = msg.sender;
         startTime = block.timestamp;
-        endTime = startTime + (_durationMinutes * 60 seconds);
+        if (_durationMinutes < 2)
+            endTime = startTime + 120 seconds;
+        else 
+            endTime = startTime + (_durationMinutes * 60 seconds);
         bids[address(0)] = 0;
         highestBindingBid = 0;
-        highestBidder = address(0);
-
+        numHighestBidders = 0;
     }
 
     modifier onlyOwner() {
@@ -39,7 +41,7 @@ contract DecentralizedAuction {
     }
 
     function placeBid(uint256 increment) external payable onlyBeforeEnd {
-        uint256 minBid = bids[highestBidder] + increment;
+        uint256 minBid = bids[highestBidders[0]] + increment;
 
         require(msg.value >= minBid, "Bid must be higher than or equal to the minimum bid");
 
@@ -51,7 +53,15 @@ contract DecentralizedAuction {
 
         if (msg.value > highestBindingBid) {
             highestBindingBid = msg.value;
-            highestBidder = msg.sender;
+            // empty highestBidders array
+            for (uint256 i = 0; i < numHighestBidders; i++) {
+                highestBidders[i] = address(0);
+            }
+            highestBidders[0] = msg.sender;
+            numHighestBidders = 1;
+        } else if (msg.value == highestBindingBid) {
+            highestBidders[numHighestBidders] = msg.sender;
+            numHighestBidders++;
         }
 
         if (bids[msg.sender] > 0) {
@@ -72,16 +82,23 @@ contract DecentralizedAuction {
         for (uint256 i = 0; i < bidders.length; i++) {
             address bidder = bidders[i];
             payable(bidder).transfer(bids[bidder]);
-            
         }
         selfdestruct(payable(owner));
     }
 
     function finalizeAuction() external onlyOwner onlyAfterEnd {
-        require(highestBidder != address(0), "No bids received");
-        
-        payable(owner).transfer(highestBindingBid);
+        require(numHighestBidders > 0, "No bids received");
+        require(bidders.length >= 3, "Need at least 3 people to bid");
+        //require(block.timestamp >= startTime + 120 seconds, "Minimum 2 minutes need to be passed before finalizing the Auction");
 
+        uint256 ownerAmount = (highestBindingBid * 10) / 100;
+
+        payable(owner).transfer(ownerAmount);
+
+        uint256 random = uint256(keccak256(abi.encodePacked(block.timestamp))) % numHighestBidders;
+        winner = highestBidders[random];
+
+        
         // for (uint256 i = 0; i < bidders.length; i++) {
         //     address bidder = bidders[i];
         //     if (bidder != highestBidder) {
@@ -90,9 +107,10 @@ contract DecentralizedAuction {
         // }
     }
 
+
     function withdrawBid() external {
         require(block.timestamp >= endTime, "Auction has not ended");
-        require(msg.sender != highestBidder, "You have won the auction");
+        require(msg.sender != winner, "You have won the auction");
 
         uint256 amount = bids[msg.sender];
         require(amount > 0, "No bids to withdraw");
@@ -100,7 +118,6 @@ contract DecentralizedAuction {
         bids[msg.sender] = 0;
 
         payable(msg.sender).transfer(amount);
-
     }
 
 }
